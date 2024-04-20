77bf30d1a79f3401697f444d6a646236f0a1ae12
Manoj <manoj09633@gmail.com>
2024-04-20 19:11:15 +0530

    Added tests for tip calculation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,22 +14,4 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'tip-calculator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('tip-calculator');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tip-calculator app is running!');
-  });
 });
--- src/app/tip-calculator/tip-calculator.component.spec.ts
+++ src/app/tip-calculator/tip-calculator.component.spec.ts
@@ -8,9 +8,9 @@ describe('TipCalculatorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TipCalculatorComponent ]
+      declarations: [TipCalculatorComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +22,37 @@ describe('TipCalculatorComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // Calculates tip and total value for a single person with default tip percentage
+  it('should calculate tip and total value for a single person with default tip percentage', () => {
+    const component = new TipCalculatorComponent();
+    component.onChange("bill");
+    expect(component.tipTotal).toBe(0);
+    expect(component.totalValue).toBe(0);
+    component.onChange("tip");
+    expect(component.tipTotal).toBe(0);
+    expect(component.totalValue).toBe(0);
+    component.onChange("people");
+    expect(component.tipTotal).toBe(0);
+    expect(component.totalValue).toBe(0);
+    component.onChange("bill");
+    expect(component.tipTotal).toBe(component.bill * (component.tip / 100));
+    expect(component.totalValue).toBe(component.bill + component.tipTotal);
+  });
+
+  it('should calculate tip and total value for a single person with 0 bill', () => {
+    const component = new TipCalculatorComponent();
+    component.bill = 0;
+    component.onChange("bill");
+    expect(component.tipTotal).toBe(0);
+    expect(component.totalValue).toBe(0);
+    component.onChange("tip");
+    expect(component.tipTotal).toBe(0);
+    expect(component.totalValue).toBe(0);
+    component.onChange("people");
+    expect(component.tipTotal).toBe(0);
+    expect(component.totalValue).toBe(0);
+  });
+
+
 });
